3ff467d7a264202f6d7c547d6112671361f4d309
Zhihao <woozhh@gmail.com>
2019-04-30 17:49:52 -0700

    try to solve safari thing

--- app/js/phone.js
+++ app/js/phone.js
@@ -2,3 +2,10 @@ import '../scss/phone.scss'
 import PhoneController from './components/PhoneController'
 
 new PhoneController()
+
+if (window.safari) {
+  window.history.pushState(null, null, window.location.href)
+  window.onpopstate = () => {
+    window.history.go(1)
+  }
+}
