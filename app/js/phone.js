80d4ee5401c4228a3c1df0af50d4fee3444bd365
Zhihao <woozhh@gmail.com>
2019-05-07 17:56:50 -0700

    provider fix

--- app/js/phone.js
+++ app/js/phone.js
@@ -3,7 +3,7 @@ import angular from 'angular'
 import PhoneController from './components/PhoneController'
 
 const phoneApp = angular.module('phoneApp', [])
-phoneApp.controller('PhoneController', PhoneController)
+phoneApp.controller('PhoneController', ['$scope', '$element', PhoneController])
 
 if (/iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream) {
   window.history.pushState(null, null, window.location.href)
