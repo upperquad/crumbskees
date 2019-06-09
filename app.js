c0163a346e65b950ce11c3ac5dd8692222ceba81
Zhihao <zhihao@upperquad.com>
2019-06-09 14:50:16 -0700

    disconnect button

--- app.js
+++ app.js
@@ -34,7 +34,6 @@ wssPhone.on('connection', ws => {
 
   console.log(url.parse(ws.upgradeReq.url, true).query)
 
-
   ws.on('message', message => {
     wssPage.clients.forEach(client => {
       client.send('control,' + message)
@@ -53,6 +52,12 @@ wssAdmin.on('connection', ws => {
     if (message === 'refresh' || message === 'reset') {
       wssPage.clients.forEach(client => {
         client.send('command,' + message)
+      })
+    }
+
+    if (message === 'disconnect_all') {
+      wssPage.clients.forEach(client => {
+        client.close
       })
     }
   })
