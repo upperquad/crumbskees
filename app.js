d490b8fe77fcc9a40450edb06f60c7ba6c00ca28
Zhihao <zhihao@upperquad.com>
2019-07-29 12:41:00 -0700

    port

--- app.js
+++ app.js
@@ -10,17 +10,13 @@ const initPage = require('./wss/page.js')
 const initPhone = require('./wss/phone.js')
 const initAdmin = require('./wss/admin.js')
 
-const PORT = process.env.PORT || 3000
-const phonePage = path.join(__dirname, `${frontEndRoot}phone/index.html`)
-const gamePage = path.join(__dirname, `${frontEndRoot}app/dist/game/index.html`)
-const adminPage = path.join(__dirname, `${frontEndRoot}app/dist/admin/index.html`)
+const PORT = process.env.PORT || 8000
+const page = path.join(__dirname, `${frontEndRoot}index.html`)
 
 const server = express()
-  .get('/', (req, res) => res.sendFile(phonePage) )
-  .get('/game', (req, res) => res.sendFile(gamePage) )
-  .get('/admin', (req, res) => res.sendFile(adminPage) )
+  .get('/', (req, res) => res.sendFile(page) )
   .use('/', express.static(frontEndRoot))
-  .use((req, res) => res.sendFile(phonePage))
+  .use((req, res) => res.sendFile(page))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 const wssPage = new SocketServer({server: server, path: '/game'})
