5ffdd33425671f1d140538f06efcd4d398f1bf97
Zhihao <zhihao@upperquad.com>
2019-07-29 12:24:25 -0700

    base setup

--- app.js
+++ app.js
@@ -4,20 +4,22 @@ const express = require('express')
 const SocketServer = require('ws').Server
 const path = require('path')
 
+const frontEndRoot = process.env.USE_DIST ? 'app/dist/' : 'app/build/'
+
 const initPage = require('./wss/page.js')
 const initPhone = require('./wss/phone.js')
 const initAdmin = require('./wss/admin.js')
 
 const PORT = process.env.PORT || 3000
-const phonePage = path.join(__dirname, 'app/dist/phone/index.html')
-const gamePage = path.join(__dirname, 'app/dist/game/index.html')
-const adminPage = path.join(__dirname, 'app/dist/admin/index.html')
+const phonePage = path.join(__dirname, `${frontEndRoot}phone/index.html`)
+const gamePage = path.join(__dirname, `${frontEndRoot}app/dist/game/index.html`)
+const adminPage = path.join(__dirname, `${frontEndRoot}app/dist/admin/index.html`)
 
 const server = express()
   .get('/', (req, res) => res.sendFile(phonePage) )
   .get('/game', (req, res) => res.sendFile(gamePage) )
   .get('/admin', (req, res) => res.sendFile(adminPage) )
-  .use('/', express.static('app/dist/'))
+  .use('/', express.static(frontEndRoot))
   .use((req, res) => res.sendFile(phonePage))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
