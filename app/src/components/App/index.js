e2c93c10d8178372d4aab82e920791f8f3ae1552
Zhihao <zhihao@upperquad.com>
2019-07-29 13:01:42 -0700

    project setup

---
+++ app/src/components/App/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './style.module.scss'
+
+const App = props => {
+  const { something } = props
+  return (
+    <div>
+    123
+    </div>
+  )
+}
+
+export default App
