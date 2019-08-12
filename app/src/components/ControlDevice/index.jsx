5b1878b991c6d2e73d0e511231f79b46cb54bb61
Zhihao <zhihao@upperquad.com>
2019-08-12 14:44:27 -0700

    refactor: move logic into stage

--- app/src/components/ControlDevice/index.jsx
+++ app/src/components/ControlDevice/index.jsx
@@ -6,81 +6,16 @@ import MeetCharacterSatge from './stages/MeetCharacterStage'
 import PlayStage from './stages/PlayStage'
 import ResultStage from './stages/ResultStage'
 
-import WebSocketManager from '../../managers/WebSocketManager'
-
 const ControlDevice = props => {
   // set hasPlayed when game starts
   const [hasPlayed, setHasPlayed] = useState(false)
   const [stage, setStage] = useState('pre_connect')
-  const [token, setToken] = useState('')
-  const [connectError, setConnectError] = useState(null)
-  const [isConnecting, setIsConnecting] = useState(false)
-
-  useEffect(() => effectWebsocket(), [])
-  useEffect(() => effectWebsocketClose(setToken, setConnectError, setIsConnecting), [setConnectError])
 
   return (
     <div>
-      {stage === 'pre_connect' && (
-        <PreConnectStage
-          isConnecting={isConnecting}
-          hasPlayed={hasPlayed}
-          token={token}
-          updateToken={key => updateToken(key, token, setToken, setConnectError, setIsConnecting)}
-          errorReason={connectError}
-        />
-      )}
+      {stage === 'pre_connect' && <PreConnectStage hasPlayed={hasPlayed} /> }
     </div>
   )
-}
-
-function effectWebsocket() {
-  WebSocketManager.init('control')
-
-  const path = window.location.pathname
-  if (/^\/\d{3}$/.test(path)) {
-    const token = path.slice(1)
-    WebSocketManager.connect({token})
-  }
-
-  return () => {
-    WebSocketManager.disconnect()
-  }
-}
-
-function effectWebsocketClose(setToken, setConnectError, setIsConnecting) {
-  const onWebSocketClose = event => {
-    const { detail: { reason } } = event
-    if (reason === 'invalid_token') {
-      setConnectError('Invalid token')
-    } else if (reason === 'no_active_game') {
-      setConnectError('There\'s no active game')
-    }
-    setIsConnecting(false)
-    setToken('')
-  }
-
-  window.addEventListener('WS_CLOSE', onWebSocketClose)
-
-  return () => {
-    window.removeEventListener('WS_CLOSE', onWebSocketClose)
-  }
-}
-
-function updateToken(key, token, setToken, setConnectError, setIsConnecting) {
-  setConnectError(null)
-
-  if (key === -1) {
-    setToken(token.slice(0, -1))
-  } else {
-    const newToken = token + key
-    if (newToken.length >= 3) {
-      setIsConnecting(true)
-      WebSocketManager.connect({token: newToken})
-    }
-
-    setToken(newToken)
-  }
 }
 
 export default ControlDevice
--- app/src/components/ControlDevice/stages/PreConnectStage/index.jsx
+++ app/src/components/ControlDevice/stages/PreConnectStage/index.jsx
@@ -4,8 +4,18 @@ import styles from './style.module.scss'
 
 import Circle from '../../Circle'
 
+import WebSocketManager from '../../../../managers/WebSocketManager'
+
 const PreConnectStage = props => {
-  const { isConnecting, errorReason, hasPlayed, token, updateToken } = props
+  const [token, setToken] = useState('')
+  const [errorReason, setErrorReason] = useState(null)
+  const [isConnecting, setIsConnecting] = useState(false)
+
+  useEffect(() => effectWebsocket(), [])
+  useEffect(() => effectWebsocketClose(setToken, setErrorReason, setIsConnecting), [setToken, setErrorReason, setIsConnecting])
+
+  const { hasPlayed } = props
+
   return (
     <div className={styles.preConnect}>
       <Circle color="yellow" />
@@ -18,10 +28,10 @@ const PreConnectStage = props => {
         {isConnecting && <span className={styles.labelConnecting}>Connecting...</span>}
       </div>
       <div className={styles.tokenInput}>
-        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map(number => (
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map(key => (
           <div className={styles.tokenInputKey}
-            key={number}
-            onClick={() => updateToken(number)}>{number}</div>
+            key={key}
+            onClick={() => updateToken(key, token, setToken, setErrorReason, setIsConnecting)}>{key}</div>
         ))}
         <div
           className={styles.tokenInputKey}
@@ -36,6 +46,55 @@ const PreConnectStage = props => {
       </div>
     </div>
   )
+}
+
+function effectWebsocket() {
+  WebSocketManager.init('control')
+
+  const path = window.location.pathname
+  if (/^\/\d{3}$/.test(path)) {
+    const token = path.slice(1)
+    WebSocketManager.connect({token})
+  }
+
+  return () => {
+    WebSocketManager.disconnect()
+  }
+}
+
+function effectWebsocketClose(setToken, setErrorReason, setIsConnecting) {
+  const onWebSocketClose = event => {
+    const { detail: { reason } } = event
+    if (reason === 'invalid_token') {
+      setErrorReason('Invalid token')
+    } else if (reason === 'no_active_game') {
+      setErrorReason('There\'s no active game')
+    }
+    setIsConnecting(false)
+    setToken('')
+  }
+
+  window.addEventListener('WS_CLOSE', onWebSocketClose)
+
+  return () => {
+    window.removeEventListener('WS_CLOSE', onWebSocketClose)
+  }
+}
+
+function updateToken(key, token, setToken, setErrorReason, setIsConnecting) {
+  setErrorReason(null)
+
+  if (key === -1) {
+    setToken(token.slice(0, -1))
+  } else {
+    const newToken = token + key
+    if (newToken.length >= 3) {
+      setIsConnecting(true)
+      WebSocketManager.connect({token: newToken})
+    }
+
+    setToken(newToken)
+  }
 }
 
 export default PreConnectStage
