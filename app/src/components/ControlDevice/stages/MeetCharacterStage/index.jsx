301ec70569a91d106796547d7c92c9bb2a5993be
Celia Dagorne <>
2019-08-13 11:42:13 -0700

    style

--- app/src/components/ControlDevice/stages/MeetCharacterStage/index.jsx
+++ app/src/components/ControlDevice/stages/MeetCharacterStage/index.jsx
@@ -1,14 +1,15 @@
 import React, { useState, useEffect } from 'react'
-import styles from './style.module.scss'
+import characterVideoWhite1 from '../../../../assets/images/character-white-1.mp4'
+import "./style.module.scss"
+import "../../../../styles/common/control-device.scss";
 
 const MeetCharacterStage = props => {
-  console.log(styles.characterConfirm)
   return (
-    <section className={styles.characterConfirm}>
+    <section className="character-confirm state is-shown">
       <div className="circle circle--purple"></div>
       <div className="text"><h2>You're in!</h2><p>Say hello to your avatar.</p></div>
       <div className="block">
-        <video src="" poster="" autoPlay muted loop></video>
+        <video src={characterVideoWhite1} poster="" autoPlay muted loop></video>
       </div>
       <div className="placeholder"></div>
       <div className="button" role="button" ng-click="phoneCtrl.handleConfirm()">Ready</div>
