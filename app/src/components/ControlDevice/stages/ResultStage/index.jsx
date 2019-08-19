c0fe207f88cc6cc21d5c2c8a9666717a0f1e9580
Celia Dagorne <>
2019-08-19 14:03:28 -0700

    result page front

--- app/src/components/ControlDevice/stages/ResultStage/index.jsx
+++ app/src/components/ControlDevice/stages/ResultStage/index.jsx
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react'
-import "./style.module.scss"
+import styles from './style.module.scss'
 import characterVideoWhite1 from '../../../../assets/images/character-white-1.mp4'
 import characterVideoWhite2 from '../../../../assets/images/character-white-2.mp4'
 
+import Circle from '../../Circle'
+import MarqueeText from '../../../MarqueeText'
 
 const ResultStage = props => {
 
@@ -19,30 +21,29 @@ const ResultStage = props => {
     image: '',
     videoWhite: '',
   }
-  const resultTop = 'You won!'
-  const resultBottom = character.name
+  const resultTop = 'You won! '
+  const resultBottom = `${character.name} `
 
   return (
-    <section className="result state is-shown">
-      <div className={"circle circle--" + (result === 'won' ? character.color : (result === 'lost' ? opponent.color : 'yellow'))}></div>
-      <div className={`winner winner--${result}`}>
-        <div className="marquee alternate">
-          <span className="invert">{resultTop}&nbsp;</span><span>{resultTop}&nbsp;</span><span className="invert">{resultTop}&nbsp;</span><span>{resultTop}&nbsp;</span>
-        </div>
-        <div className="image-wrapper {{phoneCtrl.result === 'tied' ? 'image-wrapper--both' : null}}">
+    <section className={styles.result}>
+      {result === 'won' && <Circle color={character.color} />}
+      {result === 'won' && <Circle color={character.color} />}
+      {result === 'tied' && <Circle color="yellow" />}
+      <div className={`${styles.winner} ${styles.winner}--${result}`}>
+        <MarqueeText text={resultTop} duration='6s' alternate/>
+        <div className={styles.imageWrapper}>
+        {/* <div className="image-wrapper {{phoneCtrl.result === 'tied' ? 'image-wrapper--both' : null}}"> */}
           {result === 'won' && <video src={characterVideoWhite1} poster={opponent.image} playsInline autoPlay muted loop></video>}
           {result === 'tied' && <video src={characterVideoWhite1} poster={opponent.image} playsInline autoPlay muted loop></video>}
           {result === 'lost' && <video src={characterVideoWhite2} poster={opponent.image} playsInline autoPlay muted loop></video>}
         </div>
-        <div className="marquee alternate">
-          <span>{resultBottom}&nbsp;</span><span className="invert">{resultBottom}&nbsp;</span><span>{resultBottom}&nbsp;</span><span className="invert">{resultBottom}&nbsp;</span>
-        </div>
+        <MarqueeText text={resultBottom} duration='6s' alternate/>
       </div>
-      <div className="small-text">
+      <div className={styles.smallText}>
         {result === 'won' && 'Winner!'}
         {result === 'tied' && 'Tie!'}
       </div>
-      <div className="button" ng-click="phoneCtrl.playAgain()">Play again</div>
+      <div className={styles.button} ng-click="phoneCtrl.playAgain()">Play again</div>
     </section>
   )
 }
