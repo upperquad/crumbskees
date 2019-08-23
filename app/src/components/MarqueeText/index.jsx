f8f966c33731fb548a6342ef45dbb1b723eefb90
Celia Dagorne <>
2019-08-23 11:21:38 -0700

    update marquee

--- app/src/components/MarqueeText/index.jsx
+++ app/src/components/MarqueeText/index.jsx
@@ -3,11 +3,11 @@ import classNames from 'classnames'
 import styles from './style.module.scss'
 
 const MarqueeText = props => {
-  const { text, extraClassName, duration, type, invert, winner } = props
+  const { text, extraClassName, duration, type, isWhite, isAlternate } = props
   const animationDuration = duration ? duration : '12s'
 
   return (
-    <div className={classNames(styles.marquee, extraClassName, {[styles.marqueeInvert]: invert}, {[styles.marqueeWinner]: winner})}>
+    <div className={classNames(styles.marquee, extraClassName, {[styles.marqueeAlternate]: isAlternate}, {[styles.marqueeWhite]: isWhite})}>
       <div className={styles.marqueeInnerText} style={{animationDuration}}>{text}&nbsp;</div>
       <div className={styles.marqueeInnerText} style={{animationDuration}}>{text}&nbsp;</div>
       <div className={styles.marqueeInnerText} style={{animationDuration}}>{text}&nbsp;</div>
