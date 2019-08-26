04da31d5c92e3638d52d941ffb16a8bf4dd25f4d
Zhihao <zhihao@upperquad.com>
2019-08-26 15:03:19 -0700

    final stage styling only

--- app/src/components/DisplayDevice/stages/ResultStage/index.jsx
+++ app/src/components/DisplayDevice/stages/ResultStage/index.jsx
@@ -5,6 +5,7 @@ import styles from './style.module.scss'
 import MarqueeText from '../../../MarqueeText'
 
 import resultBg from '../../../../assets/images/round_3/r3-intro.mp4'
+import characterImage1 from '../../../../assets/images/character1.png'
 
 const ResultStage = props => {
   return (
@@ -12,14 +13,14 @@ const ResultStage = props => {
       <video className={styles.background} loop autoPlay muted src={resultBg} />
       <div className={styles.card}>
         <div className={styles.titleWrapper}>
-          <MarqueeText extraClassName={styles.title} text='winner!' duration='5s' />
+          <MarqueeText extraClassName={styles.title} text='winner!' isAlternate isWhite duration='5s' />
         </div>
-        <div class="final__player text-40--bold uppercase"></div>
-        <div class="final__players-img">
-          <img src="../../assets/game/images/character1.png" class="final__player-img" />
+        <div className={styles.player}>TODO: Player 1</div>
+        <div className={classNames(styles.playersImages, {[styles.playersImagesTied]: false})}>
+          <img src={characterImage1} className={styles.playerAvatar} />
         </div>
-        <div class="final__score text-120--bold">37</div>
-        <div class="final__circle" />
+        <div className={styles.score}>37</div>
+        <div className={styles.circle} />
         <div className={classNames(styles.timebar, styles.resultTimebar)} />
       </div>
     </div>
