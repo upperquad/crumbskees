81ed734aec4c9979077fc3a405569051b70ec3a3
Robin Payot <robpayot@gmail.com>
2019-08-27 18:05:35 -0700

    watch:build config

---
+++ app/webpack.common.js
@@ -0,0 +1,41 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+       {
+        test: /\.(png|svg|jpg|gif|mp4|mp3)$/,
+        use: [
+          'file-loader'
+        ]
+      }, {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader'
+        ]
+      }, {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./public/index.html",
+    })
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx', '.scss']
+  }
+};
