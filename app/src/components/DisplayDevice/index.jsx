4ee62a83cca48a2403c3c1a46baea1489255c31f
Robin Payot <robpayot@gmail.com>
2019-09-05 16:54:29 -0700

    debug false

--- app/src/components/DisplayDevice/index.jsx
+++ app/src/components/DisplayDevice/index.jsx
@@ -17,7 +17,7 @@ const STAGE_TRANSITION_OUT = 1300
 const STAGE_TRANSITION_IN = 800
 
 const DisplayDevice = () => {
-  const [stage, setStage] = useState('play')
+  const [stage, setStage] = useState('setup')
   const [bothConnected, setBothConnected] = useState(false)
   const forceUpdate = useForceUpdate()
 
