6e070763a01c56470170b5628d3c8bd949c336a0
Zhihao <zhihao@upperquad.com>
2019-10-03 12:37:00 -0700

    move cursor render logic to PlayerCursor

--- app/src/managers/PlayersManager/Player/index.js
+++ app/src/managers/PlayersManager/Player/index.js
@@ -1,9 +1,6 @@
 import getNow from '~utils/time'
-import { random } from '~utils/math'
 import WebSocketManager from '~managers/WebSocketManager'
-import { VB_WIDTH, VB_HEIGHT, GRID_UNIT } from '~constants'
-
-const POINTS_COUNT = 8
+import { VB_WIDTH } from '~constants'
 
 export default class Player {
   _score = 0
@@ -17,7 +14,6 @@ export default class Player {
       video,
       videoWhite,
     } = character
-    // this.el = el
     this.id = id
     this.lost = false
 
@@ -28,17 +24,6 @@ export default class Player {
     this.videoWhite = videoWhite
     this.image = image
     this.name = name
-
-    this.centerX = VB_WIDTH / 2 // equal to svg viewbox / 2
-    this.centerY = VB_HEIGHT / 2 // equal to svg viewbox / 2
-    this.minRadius = GRID_UNIT * 1.1 // 3.125 == 1 unit grid (1920 / 32)
-    this.maxRadius = this.minRadius + this.minRadius * 0.45
-    this.minMiddleRadius = this.minRadius + (this.maxRadius - this.minRadius) * 0.45
-    this.maxMiddleRadius = this.minRadius + (this.maxRadius - this.minRadius) * 0.55
-    this.minDuration = 500
-    this.maxDuration = 700
-
-    this.setPoints()
   }
 
   destroy = () => {
@@ -49,39 +34,6 @@ export default class Player {
     this.lost = true
   }
 
-  setPoints() {
-    this.points = []
-    const slice = (Math.PI * 2) / POINTS_COUNT
-    this.startAngle = random(0, Math.PI * 2)
-
-    for (let i = 0; i < POINTS_COUNT; i++) {
-      const margeAngle = random(0, 0.28) // i / 1.2
-      // randomize the start time of animation (we don't want the tween to go from 0 to 1, it can start directly from 0.6 for example)
-      const startAnim = getNow() + i * random(0, this.minDuration)
-      const angle = this.startAngle + i * slice + margeAngle
-      const duration = random(this.minDuration, this.maxDuration)
-
-      const point = {
-        angle,
-        duration,
-        startAnim,
-        x: this.centerX + Math.cos(angle) * random(this.minRadius, this.maxRadius),
-        y: this.centerY + Math.sin(angle) * random(this.minRadius, this.maxRadius),
-        targetMinX: this.centerX + Math.cos(angle) * random(this.minRadius, this.minMiddleRadius),
-        targetMinY: this.centerY + Math.sin(angle) * random(this.minRadius, this.minMiddleRadius),
-        targetMaxX: this.centerX + Math.cos(angle) * random(this.maxMiddleRadius, this.maxRadius),
-        targetMaxY: this.centerY + Math.sin(angle) * random(this.maxMiddleRadius, this.maxRadius),
-      }
-
-      point.startX = point.x
-      point.startY = point.y
-      point.destX = point.targetMaxX
-      point.destY = point.targetMaxY
-
-      this.points.push(point)
-    }
-  }
-
   setPower = type => {
     switch (type) {
       default:
@@ -89,7 +41,6 @@ export default class Player {
       case 'grow':
         this.updateRadius(VB_WIDTH * 0.05)
         // window.GameManager.popUpMessage('GROW', 'orange', false)
-        timeClean = 6000
         break
     }
   }
@@ -105,42 +56,34 @@ export default class Player {
 
   score = () => this._score
 
-  updateRadius(incr) {
-    for (let i = 0; i < this.points.length; i++) {
-      const point = this.points[i]
-      // Increase each points
-      // if player has grown power, increase player radius
-      const newMaxRadius = this.maxRadius + incr
-      const newMaxMiddleRadius = this.maxMiddleRadius + incr
-      const newMinRadius = this.minRadius + incr
-      const newMinMiddleRadius = this.minMiddleRadius + incr
+  // updateRadius(incr) {
+  //   for (let i = 0; i < this.points.length; i++) {
+  //     const point = this.points[i]
+  //     // Increase each points
+  //     // if player has grown power, increase player radius
+  //     const newMaxRadius = this.maxRadius + incr
+  //     const newMaxMiddleRadius = this.maxMiddleRadius + incr
+  //     const newMinRadius = this.minRadius + incr
+  //     const newMinMiddleRadius = this.minMiddleRadius + incr
 
-      point.duration += 250
+  //     point.duration += 250
 
-      point.targetMaxX = this.centerX + Math.cos(point.angle) * random(newMaxMiddleRadius, newMaxRadius)
-      point.targetMinX = this.centerX + Math.cos(point.angle) * random(newMinRadius, newMinMiddleRadius)
+  //     point.targetMaxX = this.centerX + Math.cos(point.angle) * random(newMaxMiddleRadius, newMaxRadius)
+  //     point.targetMinX = this.centerX + Math.cos(point.angle) * random(newMinRadius, newMinMiddleRadius)
 
-      point.destX = point.targetMaxX
+  //     point.destX = point.targetMaxX
 
-      point.targetMaxY = this.centerY + Math.sin(point.angle) * random(newMaxMiddleRadius, newMaxRadius)
-      point.targetMinY = this.centerY + Math.sin(point.angle) * random(newMinRadius, newMinMiddleRadius)
+  //     point.targetMaxY = this.centerY + Math.sin(point.angle) * random(newMaxMiddleRadius, newMaxRadius)
+  //     point.targetMinY = this.centerY + Math.sin(point.angle) * random(newMinRadius, newMinMiddleRadius)
 
-      point.destY = point.targetMaxY
-      point.startAnim = getNow()
-    }
+  //     point.destY = point.targetMaxY
+  //     point.startAnim = getNow()
+  //   }
 
-    setTimeout(() => { // when growing animation finish
-      for (let i = 0; i < this.points.length; i++) {
-        this.points[i].duration -= 250
-      }
-    }, 1000)
-  }
-
-  cleanPowers = () => {
-    this.updateRadius(0)
-  }
-
-  startNewRound = () => {
-    this.cleanPowers()
-  }
+  //   setTimeout(() => { // when growing animation finish
+  //     for (let i = 0; i < this.points.length; i++) {
+  //       this.points[i].duration -= 250
+  //     }
+  //   }, 1000)
+  // }
 }
