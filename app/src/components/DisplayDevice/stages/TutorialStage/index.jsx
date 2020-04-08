d79157623f3f14f9451009721049469c38dd843c
Robpayot <robpayot@gmail.com>
2020-04-08 16:47:35 -0700

    init Tutorial pixi scene

--- app/src/components/DisplayDevice/stages/TutorialStage/index.jsx
+++ app/src/components/DisplayDevice/stages/TutorialStage/index.jsx
@@ -1,128 +1,291 @@
 import React, { useState, useEffect } from 'react'
 import classNames from 'classnames'
 import { TransitionGroup, Transition } from 'react-transition-group'
+import PlayersManager from '~managers/PlayersManager'
+import styles from './style.module.scss'
+import SoundManager from '~managers/SoundManager'
 import Player1Peer from '~managers/PeerManager/Player1Peer'
 import Player2Peer from '~managers/PeerManager/Player2Peer'
-import styles from './style.module.scss'
+import { TUTORIAL_ROUND, VB_WIDTH, VB_HEIGHT, GRID_UNIT, GRID_UNIT_VW, GRID_UNIT_VH, COLORS } from '~constants'
+import { clamp, randomInt } from '~utils/math'
 
-import { DEBUG } from '~constants'
+import PixiSceneTutorial from '../PlayStage/Round/PixiSceneTutorial'
+import PlayerMessage from '../PlayStage/Round/PlayerMessage'
+import PopupMessage from '../PlayStage/Round/PopupMessage'
 
-import MarqueeText from '~components/MarqueeText'
-import DisplayFooter from '~components/DisplayFooter'
-import AutoplayVideo from '~components/AutoplayVideo'
-
-import homeBgVideo from '~assets/images/home-bg.mp4'
-import step1Img from '~assets/images/tutorial/tutorial-1.png'
-import step2Img from '~assets/images/tutorial/tutorial-2.png'
-import step3Img from '~assets/images/tutorial/tutorial-3.png'
-
-const TUTORIALS = [
-  {
-    key: 'tutorial-step-0',
-    img: step1Img,
-    text: 'It’s your job to collect the lost shapes.',
-  }, {
-    key: 'tutorial-step-1',
-    img: step2Img,
-    text: 'Tap on the shape when you see it using your phone screen.',
-  }, {
-    key: 'tutorial-step-2',
-    img: step3Img,
-    text: 'Hint! Be on the lookout for hidden powerups.',
-  },
-]
 
 const TutorialStage = props => {
-  const { bothConnected, extraClassName, onFinish, rollback } = props
-  const [step, setStep] = useState(0)
-  const [progress, setProgress] = useState(0)
+  const { extraClassName, onFinish } = props
+  const { videoBack, videoFront } = TUTORIAL_ROUND
+  const [items, setItems] = useState([])
+  const [gameState, setGameState] = useState('before-game')
+  const [message, setMessage] = useState({ messageCount: 0 })
+  const [roundScoreArray, setRoundScoreArray] = useState(() => PlayersManager.players.map(() => 0))
+  const [powerArray, setPowerArray] = useState(() => PlayersManager.players.map(() => null))
+  const [positionArray, setPositionArray] = useState(() => PlayersManager.players.map(() => ({ x: 0, y: 0 })))
+  const [tapInstructionArray, setTapInstructionArray] = useState(() => PlayersManager.players.map(() => false))
 
+  const addMessage = messageObj => {
+    setMessage(prevMessage => ({
+      ...messageObj,
+      messageCount: prevMessage.messageCount + 1,
+    }))
+  }
+
+  // Players input
   useEffect(() => {
-    const messageHandler = detail => {
-      const { type } = detail
+    const removeItems = itemsCaught => {
+      setItems(prevItems => {
+        const newItems = prevItems.filter(item => !itemsCaught.includes(item))
+        const newTargets = newItems.filter(item => item.type === 'target')
+
+        if (newTargets.length === 0) {
+          addMessage({
+            text: getEndMessage(),
+            color: COLORS.red,
+            persistent: false,
+            // onEnd: () => setGameState('after-game'),
+          })
+        }
+
+        return newItems
+      })
+    }
+
+    const handleClick = playerIndex => {
+      const itemsCaught = getItemsInCursor(items, positionArray[playerIndex], powerArray[playerIndex] === 'grow')
+
+      let targetCount = 0
+      let growFound
+      let freezeFound
+      itemsCaught.forEach(item => {
+        switch (item.type) {
+          case 'grow':
+            growFound = true
+            break
+          case 'freeze':
+            freezeFound = true
+            break
+          default:
+          case 'target':
+            targetCount += 1
+            break
+        }
+      })
+
+      if (growFound) {
+        SoundManager.grow.play()
+        setPowerArray(prevArray => {
+          prevArray[playerIndex] = 'grow'
+          return [...prevArray]
+        })
+      }
+
+      if (freezeFound) {
+        SoundManager.freeze.play()
+        setPowerArray(prevArray => {
+          prevArray[1 - playerIndex] = 'freeze'
+          return [...prevArray]
+        })
+      }
+
+      if (targetCount > 0) {
+        setRoundScoreArray(prevScoreArray => {
+          prevScoreArray[playerIndex] += targetCount
+          return prevScoreArray
+        })
+        SoundManager.score.play()
+      }
+
+      if (itemsCaught.length > 0) {
+        removeItems(itemsCaught)
+      }
+    }
+
+    const messageHandler = (detail, playerIndex) => {
+      const { data, type } = detail
 
       switch (type) {
-        case 'skip_tutorial':
-          onFinish()
+        case 'cursor_move': {
+          const { x, y } = data
+          setPositionArray(prevPositionArray => {
+            const positionObj = prevPositionArray[playerIndex]
+
+            positionObj.x = clamp(positionObj.x + parseFloat(x, 10), -0.5, 0.5)
+            positionObj.y = clamp(positionObj.y + parseFloat(y, 10), -0.5, 0.5)
+
+            return prevPositionArray
+          })
           break
+        }
+        case 'click': {
+          // prevent clicks after game ends
+          if (gameState !== 'after-game') {
+            handleClick(playerIndex)
+          }
+          break
+        }
         default:
           break
       }
     }
-    Player1Peer.addSubscriber('MESSAGE', messageHandler)
-    Player2Peer.addSubscriber('MESSAGE', messageHandler)
+
+    const player1MessageHandler = detail => messageHandler(detail, 0)
+    const player2MessageHandler = detail => messageHandler(detail, 1)
+    Player1Peer.addSubscriber('MESSAGE', player1MessageHandler)
+    Player2Peer.addSubscriber('MESSAGE', player2MessageHandler)
 
     return () => {
-      Player1Peer.removeSubscriber('MESSAGE', messageHandler)
-      Player2Peer.removeSubscriber('MESSAGE', messageHandler)
+      Player1Peer.removeSubscriber('MESSAGE', player1MessageHandler)
+      Player2Peer.removeSubscriber('MESSAGE', player2MessageHandler)
     }
-  }, [onFinish])
 
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [gameState, items])
+
+  // Grid setup
+  useEffect(() => setupGrid(setItems), [])
+
+  // tap instruction
+  const zeroScorePlayers = PlayersManager.players.filter(player => player.score && player.score() === 0)
   useEffect(() => {
-    const progressInterval = setInterval(() => {
-      setProgress(counter => {
-        if (counter === 33) {
-          setStep(1)
-        } else if (counter === 66) {
-          setStep(2)
-        } else if (counter === 100) {
-          clearInterval(progressInterval)
-          // so we don't kick off the next stage when trying to rollback
-          if (bothConnected) {
-            onFinish()
+    if (zeroScorePlayers.length) {
+      const tapInstructionInterval = setInterval(() => {
+        const newTapInstructionArray = []
+        PlayersManager.players.forEach((player, index) => {
+          if (player.score() === 0) {
+            const itemsInCursor = getItemsInCursor(items, positionArray[index], powerArray[index] === 'grow')
+            const targetsInCursor = itemsInCursor.filter(item => item.type === 'target')
+            newTapInstructionArray.push(targetsInCursor.length > 0)
+          } else {
+            newTapInstructionArray.push(false)
           }
-          return counter
-        }
-        return counter + 1
-      })
-    }, DEBUG ? 1 : 100)
+        })
+        setTapInstructionArray(newTapInstructionArray)
+      }, 400)
 
-    return () => {
-      clearInterval(progressInterval)
+      return () => {
+        setTapInstructionArray(prevArray => prevArray.map(() => false))
+        clearInterval(tapInstructionInterval)
+      }
     }
-  }, [onFinish, bothConnected])
 
-  useEffect(() => {
-    if (!bothConnected) {
-      rollback()
-    }
-  }, [bothConnected, rollback])
+    return undefined
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [items, zeroScorePlayers.length])
+
+  function cancelPower(index) {
+    setPowerArray(prevArray => {
+      prevArray[index] = null
+      return [...prevArray]
+    })
+  }
 
   return (
     <div className={classNames(styles.tutorial, extraClassName)}>
-      <AutoplayVideo extraClassName={styles.video} src={homeBgVideo} />
-      <MarqueeText extraClassName={styles.welcome} text="Welcome to The Upperquadrant -" duration="12s" />
       <div className={styles.tutorialCard}>
-        <div className={styles.tutorialHeader}>Your instructions</div>
-        <TransitionGroup>
-          <Transition
-            key={TUTORIALS[step].key}
-            timeout={{ enter: 150, exit: 300 }}
-          >
-            {status => (
-              <div
-                className={classNames(styles.tutorialContent, {
-                  [styles.tutorialContentTransitioning]:
-                    status === 'entering' || status === 'exiting' || status === 'exited',
-                })}
-              >
-                <img className={styles.tutorialImage} src={TUTORIALS[step].img} alt="" />
-                <div className={styles.tutorialText}>{TUTORIALS[step].text}</div>
-              </div>
-            )}
-          </Transition>
-        </TransitionGroup>
-        <div className={styles.timebar}>
-          <div
-            className={styles.timebarColor}
-            style={{ transform: `scaleX(${progress / 100})` }}
-          />
-          <div className={styles.timebarText}>{progress}</div>
-        </div>
+        <div className={styles.tutorialHeader}>Warm up</div>
       </div>
-      <DisplayFooter />
+      <div className={styles.gameZone}>
+        <PixiSceneTutorial
+          videoFront={videoFront}
+          videoBack={videoBack}
+          positions={positionArray}
+          powers={powerArray}
+          cancelPower={cancelPower}
+          items={items}
+          gameState={gameState}
+        />
+        {PlayersManager.players.map((player, index) => (
+          <PlayerMessage
+            power={powerArray[index]}
+            position={positionArray[index]}
+            color={player.color}
+            roundScore={roundScoreArray[index]}
+            tapInstruction={tapInstructionArray[index]}
+          />
+        ))}
+        <PopupMessage
+          color={message.color}
+          x={message.x}
+          y={message.y}
+          persistent={message.persistent}
+          text={message.text}
+          messageCount={message.messageCount}
+          onEnd={message.onEnd}
+        />
+      </div>
     </div>
   )
+}
+
+function setupGrid(setItems) {
+  // REVIEW: this is really inefficient
+  const grid = []
+  const { gridCols, gridLines, itemImage, numItems, power } = TUTORIAL_ROUND
+  for (let i = 0; i < gridCols; i++) {
+    for (let j = 0; j < gridLines; j++) {
+      const obj = { x: i, y: j }
+      grid.push(obj)
+    }
+  }
+
+  const items = []
+
+  if (power) {
+    const item = createItem(grid, power)
+    items.push(item)
+  }
+
+  for (let i = 0; i < numItems; i++) {
+    const item = createItem(grid, { image: itemImage })
+    items.push(item)
+  }
+
+  setItems(items)
+}
+
+function createItem(grid, powerItem) {
+  const { gridCols, gridLines } = TUTORIAL_ROUND
+  // randomize
+  const rd = randomInt(0, grid.length - 1)
+  const x = grid[rd].x / gridCols + GRID_UNIT_VW / 200 // 200?
+  const y = grid[rd].y / gridLines + GRID_UNIT_VH / 200
+  grid.splice(rd, 1)
+
+  const size = GRID_UNIT
+
+  const { color = null, image, type = 'target' } = powerItem
+
+  return {
+    x,
+    y,
+    size,
+    image,
+    type,
+    color,
+  }
+}
+
+function getItemsInCursor(items, position, isGrown) {
+  const xPx = position.x + 0.5
+  const yPx = position.y + 0.5
+
+  const minDistanceSquare = isGrown ? 185 ** 2 : 95 ** 2
+
+  return items.filter(item => {
+    const itemXPx = item.x
+    const itemYPx = item.y
+    const distanceSquare = ((xPx - itemXPx) * VB_WIDTH) ** 2 + ((yPx - itemYPx) * VB_HEIGHT) ** 2
+
+    return distanceSquare <= minDistanceSquare
+  })
+}
+
+function getEndMessage() {
+  // TODO: we need a larger pool of messages, this is top priority
+  const phrases = ['Dope.', 'Good job!', 'Awesome!']
+  return phrases[Math.floor(Math.random() * phrases.length)]
 }
 
 export default TutorialStage
