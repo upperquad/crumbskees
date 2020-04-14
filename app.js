04c89a7ed30b3259fa6c4a3262e8f6c1154c209b
Robpayot <robpayot@gmail.com>
2020-04-14 16:07:40 -0700

    Test parameters

--- app.js
+++ app.js
@@ -15,10 +15,41 @@ const PORT = process.env.PORT || 8000
 const page = path.join(__dirname, `${frontEndRoot}index.html`)
 
 // replace the special strings with server generated strings
-const replaceMeta = (data, query = null) => {
-  data = data.replace(/\__OG_TITLE__/g, 'Home Page')
-  data = data.replace(/\__OG_DESCRIPTION__/g, "Home page description")
-  data = data.replace(/\__OG_IMAGE__/g, 'https://i.imgur.com/V7irMl8.png')
+const replaceMeta = (data, req, isShared = false) => {
+  const url = `${req.protocol}://${req.get('host') + req.originalUrl}`
+  // All copy below are just placeholder and needs to be replaced
+  let title = 'Upperquad game' // Should we modify the title when sharing or always keep the same?
+  let description = 'Come play this amazing game!'
+  let image = 'https://i.imgur.com/V7irMl8.png'
+
+  if (isShared) {
+    const { score, player, result } = req.query
+    switch (result) {
+      case 'win':
+        description = `I won and scored ${score}pts at this game!`
+        break
+      case 'lose':
+        description = `I lost at the game but I'll have my revenge!`
+        break
+      case 'tied':
+        description = `I tied, ${score}pts!`
+        break
+    }
+
+    switch(player) {
+      case '1':
+        image = 'https://dummyimage.com/600x400/6c12e3/fff'
+        break
+      case '2':
+        image = 'https://dummyimage.com/600x400/ff4046/fff'
+        break
+    }
+  }
+
+  data = data.replace(/\__OG_TITLE__/g, title)
+  data = data.replace(/\__OG_DESCRIPTION__/g, description)
+  data = data.replace(/\__OG_IMAGE__/g, image)
+  data = data.replace(/\__OG_URL__/g, url)
 
   return data
 }
@@ -30,17 +61,17 @@ const server = express()
       if (err) {
         return console.log(err);
       }
-      const result = replaceMeta(data)
+      const result = replaceMeta(data, req)
       res.send(result);
     });
   })
-  .get('/shared', (req, res) => {
+  .get('/share', (req, res) => {
     // read in the index.html file
     fs.readFile(page, 'utf8', (err, data) => {
       if (err) {
         return console.log(err);
       }
-      const result = replaceMeta(data, req.query)
+      const result = replaceMeta(data, req, true)
       res.send(result);
     });
   })
