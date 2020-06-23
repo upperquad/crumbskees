f706feb64c5466a8ef9ec3b2bc83d363be2456cd
Zhihao <zhihao@upperquad.com>
2020-06-23 13:20:21 -0700

    upload less stuff to app engine

--- app/src/index.jsx
+++ app/src/index.jsx
@@ -2,6 +2,5 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './styles/base.scss'
 import App from './components/App'
-import './globals' // import global vars
 
 ReactDOM.render(<App />, document.getElementById('root'))
