b4c50bd9699360b68b046b6f0d3e56a85f4c74af
Zhihao <zhihao@upperquad.com>
2020-06-24 23:11:55 -0700

    enforce https

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const express = require('express')
+const helmet = require('helmet')
 const SocketServer = require('ws').Server
 const SocketPeer = require('socketpeer')
 const path = require('path')
@@ -14,7 +15,12 @@ const initControl = require('./wss/control.js')
 const PORT = process.env.PORT || 8000
 const page = path.join(__dirname, `${frontEndRoot}index.html`)
 
+const sixtyDaysInSeconds = 5184000
+
 const server = express()
+  .use(helmet.hsts({
+    maxAge: sixtyDaysInSeconds
+  }))
   .use('/', express.static(frontEndRoot))
   .use((req, res) => res.sendFile(page))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
